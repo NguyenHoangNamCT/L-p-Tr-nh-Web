edb8a575c71b0b4934d2603278778aae65e95539
NguyenHoangNamCT <nguyenhoangnam1710.ct@gmail.com>
2022-10-24 11:22:44 +0700

    hoan thanh bai 2

--- Buoi06_GiaiBaiTapPhp/Bai02/view.php
+++ Buoi06_GiaiBaiTapPhp/Bai02/view.php
@@ -17,34 +17,23 @@
 <body>
 	<table>
 		<tr><th>STT</th><th>Hình ảnh</th><th>Tên sách</th><th>Đơn giá</th><th>NXB</th></tr>
-
 			<?php
-			$servername = "localhost";
-			$username = "root";
-			$password = "vertrigo";
-			$dbname = "qlsach";
-
-			//Kết nối
-			$link = mysqli_connect($servername, $username, $password);//Kết nối
-			mysqli_select_db($link, $dbname);//chọn database
-			
-			//Sử lý truy vấn
-			mysqli_set_charset($link, 'latin1');//data base ban đầu là utf-8 nhưng dữ liệU bên trong nó là latin1 nên phải chuyển về latin 1 trước khi thực hiện câu truy vấn đề không bị lỗi font
-			$kq = mysqli_query($link, 'SELECT * FROM `sach` WHERE 1');//truy vấn xong gán vô biến kq
-
-			//Hiện lên table
-			$stt = 1;
-			while($dong = mysqli_fetch_array($kq))
-			{
-				echo 
-					'<tr>
-						<td>'.$stt++.'</td>
-						<td> <img src="'.$dong['hinhanh'].'"></td>
-						<td>'.$dong['tensach'].'</td>
-						<td>'.number_format($dong['dongia']).'</td>
-						<td>'.$dong['nxb'].'</td>
-					</tr>';//Cái hình thì để dô thẻ img
-			}
+				$link = mysqli_connect('127.0.0.1', 'root', 'vertrigo');
+				mysqli_select_db($link, 'qlsach');
+				mysqli_set_charset($link, 'latin1');
+				$kq = mysqli_query($link, "select * from sach");
+				$dem = 1;
+				while($dong = mysqli_fetch_array($kq)){
+					echo'
+						<tr>
+							<td>'.$dem++.'</td>
+							<td><img src="'.$dong['hinhanh'].'" alt="Không tìm thấy hình ảnh"></td>
+							<td>'.$dong['tensach'].'</td>
+							<td>'.number_format($dong['dongia']).'</td>
+							<td>'.$dong['nxb'].'</td>
+						</tr>
+					';
+				}
 			?>
 	</table>
 </body>
